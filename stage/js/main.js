1224c5c1e864d99880b5af1733cf24390fc7b06a
AHMED ALGOHRY <ah.algohry@gmail.com>
2020-09-07 22:57:50 +0300

    Lesson 18

--- stage/js/main.js
+++ stage/js/main.js
@@ -13,4 +13,42 @@ $(function() {
             .next(".child-links")
             .slideToggle();
     });
+    //(Open / Close ) Full Screen
+    $(".toggle-fullscreen").on("click", function() {
+        $(this).toggleClass("full-screen");
+        if ($(this).hasClass('full-screen')) { // Page is now Full Screen
+            openFullscreen();
+        } else { // Page is not Full Screen
+            closeFullscreen();
+        }
+    });
+
 });
+
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+        document.msExitFullscreen();
+    }
+}
